1872b956d17d9d9c93cd5de5bf388f915c8c9324
matmal00 <mateusz.malec@microsoft.wsei.edu.pl>
2023-09-30 17:19:19 +0200

    feat: calc on button click

--- lab1/script.js
+++ lab1/script.js
@@ -1,1 +1,35 @@
-console.log("test");
+const inputs = Array.from(document.querySelectorAll("input"));
+const countBtn = document.querySelector("#count-btn");
+const sumResult = document.querySelector("#sum");
+const averageResult = document.querySelector("#average");
+const minResult = document.querySelector("#min");
+const maxResult = document.querySelector("#max");
+
+const countSum = () => {
+  const sum = inputs.reduce((acc, curr) => acc + +curr.value, 0);
+  sumResult.textContent = sum;
+
+  return sum;
+};
+
+const countAverage = (sum) => {
+  const average = sum / +inputs.length;
+  averageResult.textContent = average;
+};
+
+const getMin = () => {
+  const min = Math.min(...inputs.map((input) => +input.value));
+  minResult.textContent = min;
+};
+
+const getMax = () => {
+  const max = Math.max(...inputs.map((input) => +input.value));
+  maxResult.textContent = max;
+};
+
+countBtn.addEventListener("click", () => {
+  const sum = countSum();
+  countAverage(sum);
+  getMin();
+  getMax();
+});
